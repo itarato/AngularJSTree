c758471dd0f5880bb308d5cc71e9f0050c4bce9a
Peter Arato <it.arato@gmail.com>
2013-01-10 22:51:38 +0100

    Add files.

---
+++ mindmap.js
@@ -0,0 +1,20 @@
+function MindMap($scope) {
+  $scope.tree = {'title': 'Root'};
+
+  $scope.add_child = function(parent_node, title) {
+    var child_node = {'title': title};
+
+    if (parent_node.children) {
+      parent_node.children.push(child_node);
+    }
+    else {
+      parent_node.children = [child_node];
+    }
+  };
+
+  $scope.delete_child = function(parent_node, p) {
+    delete p[parent_node];
+    // console.log('del');
+    // console.log(p);
+  };
+}
